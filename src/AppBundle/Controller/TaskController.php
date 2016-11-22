66f473e89a2c5a69a6d2a211f8dc90a7fe460e1f
Rodrigo Ruotolo <roderickruotolo@gmail.com>
2016-11-22 18:44:49 -0200

    alteracao

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
-use AppBundle\Entity\Task;
+//use AppBundle\Entity\Task;
 use AppBundle\Repository\TaskRepository;
 
 
@@ -16,7 +16,7 @@ use AppBundle\Repository\TaskRepository;
 class TaskController extends Controller
 {
 
-    protected $taskRepository;
+    private $taskRepository;
 
     public function __construct(TaskRepository $taskRepository)
     {
@@ -24,31 +24,31 @@ class TaskController extends Controller
     }
 
     /**
-     * @Route("/list", name="list_task")
+     * @Route("/list", name="list_task", service="controller.task")
      */
     public function listAction()
     {
-        $taskRepository = $this->getDoctrine()->getRepository("AppBundle:Task");
-        $tasks = $taskRepository->findAll();
+        
+        $tasks = $this->taskRepository->findAll();
         return $this->render('AppBundle:Task:list.html.twig', array(
             "tasks" => $tasks
         ));
     }
 
     /**
-     * @Route("/view/{id}", name="view_task")
+     * @Route("/view/{id}", name="view_task", service="controller.task")
      */ 
     public function viewAction(int $id)
     {
-        $taskRepository = $this->getDoctrine()->getRepository("AppBundle:Task");
-        $task = $taskRepository->find($id);
+
+        $task = $this->taskRepository->find($id);
         return $this->render('AppBundle:Task:view.html.twig', array(
             "task" => $task
         ));
     }
 
     /**
-     * @Route("/save", name="save_task")
+     * @Route("/save", name="save_task", service="controller.task")
      * @Method("POST")
      */
     public function saveAction(Request $request) {
@@ -60,9 +60,9 @@ class TaskController extends Controller
         
         if($form->isSubmitted() && $form->isValid()) {
             
-            $taskRepository = $this->getDoctrine()->getRepository("AppBundle:Task");
 
-            $taskRepository->merge($task);
+
+            $this->taskRepository->merge($task);
 
             return $this->redirectToRoute("list_task");
         }
@@ -73,7 +73,7 @@ class TaskController extends Controller
     }
 
     /**
-     * @Route("/form", name="form_task")
+     * @Route("/form", name="form_task", service="controller.task")
      */
     public function formAction()
     {
@@ -86,12 +86,12 @@ class TaskController extends Controller
     }
 
      /**
-     * @Route("/edit/{id}", name="edit_task")
+     * @Route("/edit/{id}", name="edit_task", service="controller.task")
      */
     public function editAction(int $id) {
 
-        $taskRepository = $this->getDoctrine()->getRepository("AppBundle:Task");
-        $task = $taskRepository->find($id);
+        
+        $task = $this->taskRepository->find($id);
         $form = $this->createForm("AppBundle\Form\TaskType",$task);
         return $this->render('AppBundle:Task:form.html.twig', array(
             'form' => $form->createView()
@@ -100,11 +100,11 @@ class TaskController extends Controller
     }
 
      /**
-     * @Route("/delete/{id}", name="delete_task")
+     * @Route("/delete/{id}", name="delete_task", service="controller.task")
      */
     public function deleteAction(int $id) {
-        $taskRepository = $this->getDoctrine()->getRepository("AppBundle:Task");
-        $task = $taskRepository->find($id);
+        
+        $task = $this->taskRepository->find($id);
         $taskRepository->remove($task);
         return $this->redirectToRoute("list_task");
     }
